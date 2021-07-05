50976f6597a5a903e417d38d63007579ca445d4c
LeonardoRSantos <leonardoworkbench@gmail.com>
2021-07-05 15:39:16 -0300

    AP3POO

---
+++ app/src/Intensiva.java
@@ -0,0 +1,23 @@
+public class Intensiva {
+    // atributos
+    private String dataInic;
+    private String dataFim;
+
+    // getters e setters
+    public String getDataInic() {
+        return dataInic;
+    }
+
+    public void setDataInic(String dataInic) {
+        this.dataInic = dataInic;
+    }
+
+    public String getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(String dataFim) {
+        this.dataFim = dataFim;
+    }
+
+}
